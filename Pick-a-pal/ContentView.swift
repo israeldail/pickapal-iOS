1292788c5a283853ccb401a55f93fac22d758327
israeldail <israeldail2@gmail.com>
2025-07-05 18:45:38 -0400

    finished styling and app function

--- Pick-a-pal/ContentView.swift
+++ Pick-a-pal/ContentView.swift
@@ -15,13 +15,28 @@ struct ContentView: View {
     
     var body: some View {
         VStack {
+            
+            VStack(spacing: 8) {
+                            Image(systemName: "person.3.sequence.fill")
+                    .foregroundStyle(.tint)
+                    .symbolRenderingMode(.hierarchical)
+                            Text("Pick-a-Pal")
+                        }
+                        .font(.title)
+                        .bold()
+
+            
             Text(pickedName.isEmpty ? " " : pickedName)
+                .font(.title2)
+                .bold()
+                .foregroundStyle(.tint)
             
             List {
                 ForEach(names, id: \.description) { name in
                         Text(name)
                 }
             }
+            .clipShape(RoundedRectangle(cornerRadius: 8))
             // $nameToAdd binds variable to this UI element
             TextField("Add name", text: $nameToAdd)
                 .autocorrectionDisabled()
@@ -36,7 +51,7 @@ struct ContentView: View {
             Toggle("Remove when picked" , isOn: $shouldRemovePickedName)
             
             // button for picking a random name
-            Button("Pick a random name") {
+            Button {
                 if let randomName = names.randomElement() {
                     pickedName = randomName
                     // removes name if toggle is on
@@ -48,7 +63,13 @@ struct ContentView: View {
                 } else {
                     pickedName = ""
                 }
+            } label: {
+                Text("Pick a random name")
+                .padding(.vertical, 8)
+                .padding(.horizontal, 16)
             }
+            .buttonStyle(.borderedProminent)
+            .font(.title2)
         }
         .padding()
     }
