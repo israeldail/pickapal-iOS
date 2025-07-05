239243dd3838dbe6d819216b36881ccf35ac4fe3
israeldail <israeldail2@gmail.com>
2025-07-05 17:56:21 -0400

    Initial Commit

---
+++ Pick-a-pal/Pick_a_palApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pick_a_palApp.swift
+//  Pick-a-pal
+//
+//  Created by Israel Dail on 7/5/25.
+//
+
+import SwiftUI
+
+@main
+struct Pick_a_palApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
